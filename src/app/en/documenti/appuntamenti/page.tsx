2e7279926bef89aff59e8a2eebb2357e90c62700
rzirafi87-arch <rzirafi87@gmail.com>
2025-11-09 11:59:47 +0100

    auto: save 2025-11-09 11:59:46

--- src/app/en/documenti/appuntamenti/page.tsx
+++ src/app/en/documenti/appuntamenti/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { getBrowserClient } from "@/lib/supabaseBrowser";
 import { useTranslations } from "next-intl";
 import React, { useEffect, useState } from "react";
