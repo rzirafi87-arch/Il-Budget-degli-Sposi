d511df85488eb51d56adb84338b7e3ddabaaae31
rzirafi87-arch <rzirafi87@gmail.com>
2026-09-02 23:06:43 +0200

    feat(i18n): update src/app/robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,8 +11,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: [
           "/",
           "/it",
-          "/en",
-          "/mx",
           "/chi-siamo",
           "/come-funziona",
           "/contatti",
